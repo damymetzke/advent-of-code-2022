e27ff38cef54664cca2d27aad8900747daafd770
Damy Metzke <damy.metzke@gmail.com>
2022-12-03 15:49:20 +0100

    Solution day 3 part 2

--- days/3/main.go
+++ days/3/main.go
@@ -18,22 +18,26 @@ func GetInput() string {
 	return string(data)
 }
 
+func CalculatePriorityFromAscii(value byte) uint32 {
+	if value >= 97 && value < 123 {
+		return uint32(value - 96)
+	} else if value >= 65 && value < 91 {
+		return uint32(value - 38)
+	}
+	log.Fatalf("Invalid character '%v'", value)
+	return 0
+}
+
 // This function will calculate the priority of a specific string
 func CalculatePriority(input string) uint32 {
 
-  // Loop throug left side
+	// Loop throug left side
 	for i := 0; i < len(input)/2; i++ {
-    // Loop throug right side
+		// Loop throug right side
 		for j := len(input) / 2; j < len(input); j++ {
 			if input[i] == input[j] {
-        // Convert ascii to priority
-				if input[i] >= 97 && input[i] < 123 {
-					return uint32(input[i] - 96)
-				} else if input[i] >= 65 && input[i] < 91 {
-					return uint32(input[i] - 38)
-				} else {
-					log.Fatalf("Invalid character '%v'", input[i])
-				}
+				// Convert ascii to priority
+        return CalculatePriorityFromAscii(input[i])
 			}
 		}
 	}
@@ -42,21 +46,58 @@ func CalculatePriority(input string) uint32 {
 	return 0
 }
 
+func CalculateGroupPriority(first string, second string, third string) uint32 {
+	// Loop through first
+	for _, firstValue := range first {
+		for _, secondValue := range second {
+			if firstValue == secondValue {
+				for _, thirdvalue := range third {
+					if firstValue == thirdvalue {
+						return CalculatePriorityFromAscii(byte(firstValue))
+					}
+				}
+				break
+			}
+		}
+	}
+	log.Fatalf("No common item '%v' '%v' '%v'", first, second, third)
+	return 0
+}
+
 func main() {
 	var result uint32
+	var groupResult uint32
 
 	var waitGroup sync.WaitGroup
 
-  // Go though all rucksacks
-	for _, rucksack := range strings.Split(GetInput(), "\n") {
+	// Go though all rucksacks
+	var first string
+	var second string
+
+	for i, rucksack := range strings.Split(GetInput(), "\n") {
+		// Decided to try using goroutines
+		// This went as well as you would expect
 		waitGroup.Add(1)
-    value := rucksack
-    // Decided to try using goroutines
-    // This went as well as you would expect
+		value := rucksack
 		go func() {
 			atomic.AddUint32(&result, CalculatePriority(value))
 			waitGroup.Done()
 		}()
+
+		// Check 3 at the time using modulo logic
+		if i%3 == 0 {
+			first = rucksack
+		} else if i%3 == 1 {
+			second = rucksack
+		} else {
+			waitGroup.Add(1)
+      firstValue := first
+      secondValue := second
+			go func() {
+				atomic.AddUint32(&groupResult, CalculateGroupPriority(firstValue, secondValue, value))
+				waitGroup.Done()
+			}()
+		}
 	}
 
 	waitGroup.Wait()
@@ -64,4 +105,5 @@ func main() {
 	fmt.Println("=-= PART 1 =-=")
 	fmt.Println(result)
 	fmt.Println("=-= PART 2 =-=")
+	fmt.Println(groupResult)
 }
