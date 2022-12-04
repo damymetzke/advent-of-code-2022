173f02197594d86b862ec86bb724500059d98bbd
Damy Metzke <damy.metzke@gmail.com>
2022-12-04 12:46:28 +0100

    Solve day 4 part 1

--- days/4/main.go
+++ days/4/main.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
+	"sync"
+	"sync/atomic"
 )
 
 func GetInput() string {
@@ -15,8 +19,37 @@ func GetInput() string {
 	return string(data)
 }
 
+func IsOverlapping(ranges string) bool {
+	// Split up the string, replace works here because format is predictable
+	splits := strings.Split(strings.Replace(ranges, ",", "-", 1), "-")
+	leftMin, _ := strconv.ParseInt(splits[0], 10, 16)
+	leftMax, _ := strconv.ParseInt(splits[1], 10, 16)
+	rightMin, _ := strconv.ParseInt(splits[2], 10, 16)
+	rightMax, _ := strconv.ParseInt(splits[3], 10, 16)
+
+	// Check if any of the ranges is fully contained in the other
+	return leftMin >= rightMin && leftMax <= rightMax || leftMin <= rightMin && leftMax >= rightMax
+}
+
 func main() {
-	fmt.Println(GetInput())
+	var waitGroup sync.WaitGroup
+	var totalOverlapping uint32
+
+	for _, ranges := range strings.Split(GetInput(), "\n") {
+		waitGroup.Add(1)
+		rangesValue := ranges
+		// More goroutines, getting the hang of this now :)
+		go func() {
+			if IsOverlapping(rangesValue) {
+				atomic.AddUint32(&totalOverlapping, 1)
+			}
+			waitGroup.Done()
+		}()
+	}
+
+	waitGroup.Wait()
+
 	fmt.Println("=-= PART 1 =-=")
+	fmt.Println(totalOverlapping)
 	fmt.Println("=-= PART 2 =-=")
 }
