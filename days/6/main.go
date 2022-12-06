602a5f1f8d1d21fc9e9a87784b79813ff170a9e4
Damy Metzke <damy.metzke@gmail.com>
2022-12-06 13:28:23 +0100

    Solve day 6 part 1

--- days/6/main.go
+++ days/6/main.go
@@ -15,8 +15,27 @@ func GetInput() string {
 	return string(data)
 }
 
+func areUnique(values [4]byte) bool {
+	return values[0] != values[1] &&
+		values[0] != values[2] &&
+		values[0] != values[3] &&
+		values[1] != values[2] &&
+		values[1] != values[3] &&
+		values[2] != values[3]
+}
+
 func main() {
-  fmt.Println(GetInput())
+
+	input := GetInput()
+
+	memory := [4]byte{input[0], input[1], input[2], input[3]}
+
+  var i int
+  for i = 4; !areUnique(memory); i++ {
+    memory[i % 4] = input[i]
+  }
+
 	fmt.Println("=-= PART 1 =-=")
+  fmt.Println(i)
 	fmt.Println("=-= PART 2 =-=")
 }
