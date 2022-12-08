56485cda73adf86dd60b1a0ad0d6110181160d44
Damy Metzke <damy.metzke@gmail.com>
2022-12-08 12:39:55 +0100

    Solve day 8 part 1

--- days/8/main.go
+++ days/8/main.go
@@ -4,11 +4,19 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
+	"strings"
 )
 
-type DirData struct {
-	fileSizes        int64
-	childDirectories []string
+// A direction to check in
+type LookDirection struct {
+  // The amount of trees to check,
+  // used for simple optimisation
+	amountToCheck int
+  // Deltas will be a combination of 0 and 1/-1
+  // This will be used to iterate orthogonally through the grid
+	deltaX        int
+	deltaY        int
 }
 
 func GetInput() string {
@@ -20,8 +28,94 @@ func GetInput() string {
 	return string(data)
 }
 
+// Thank you ChatGPT
+func convertTo2DByteArray(input string) [][]byte {
+	// Split the input string into a slice of strings, where
+	// each string represents a row in the grid.
+	rows := strings.Split(input, "\n")
+
+	// Create a 2D byte array with the same dimensions as the
+	// input grid.
+	grid := make([][]byte, len(rows))
+	for i := range grid {
+		grid[i] = make([]byte, len(rows[i]))
+	}
+
+	// Iterate over the rows and convert each string into a
+	// slice of bytes.
+	for i, row := range rows {
+		for j := range row {
+			// ChatGPT tried to parse the int, I don't care so I removed the parsing
+			// The values should be ordered the same
+			grid[i][j] = row[j]
+		}
+	}
+
+	return grid
+}
+
+func incrementForDirection(x *int, y *int, direction LookDirection) {
+	*x += direction.deltaX
+	*y += direction.deltaY
+}
+
 func main() {
-  fmt.Println(GetInput())
+	grid := convertTo2DByteArray(GetInput())
+	// Width and height, easy optimisation
+	width := len(grid[0])
+	height := len(grid)
+
+	var result int
+
+	for i, row := range grid {
+		for j, tree := range row {
+			directions := []LookDirection{
+				{
+					amountToCheck: j,
+					deltaX:        -1,
+					deltaY:        0,
+				},
+				{
+					amountToCheck: width - j - 1,
+					deltaX:        1,
+					deltaY:        0,
+				},
+				{
+					amountToCheck: i,
+					deltaX:        0,
+					deltaY:        -1,
+				},
+				{
+					amountToCheck: height - i - 1,
+					deltaX:        0,
+					deltaY:        1,
+				},
+			}
+
+			sort.Slice(directions, func(i, j int) bool {
+				return directions[i].amountToCheck < directions[j].amountToCheck
+			})
+
+      // Test all directions
+		labelDirection:
+			for _, direction := range directions {
+				for y, x := i+direction.deltaY, j+direction.deltaX; y >= 0 && y < height && x >= 0 && x < width; incrementForDirection(&x, &y, direction) {
+          // In this case a tree is blocking the view
+          // So try the next direction
+					if grid[y][x] >= tree {
+						continue labelDirection
+					}
+				}
+
+        // If at least 1 loop was successful there is a clear path
+        // The tree is visible so add 1 to the result
+				result++
+				break
+			}
+		}
+	}
+
 	fmt.Println("=-= PART 1 =-=")
+	fmt.Println(result)
 	fmt.Println("=-= PART 2 =-=")
 }
