c145db7c2a2729db229f6ac72f68106117fce99c
Damy Metzke <damy.metzke@gmail.com>
2022-12-09 10:15:18 +0100

    Solve day 9 part 1

--- days/9/main.go
+++ days/9/main.go
@@ -4,9 +4,15 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"sort"
+	"strconv"
 	"strings"
 )
+
+type Position struct {
+	x int
+	y int
+}
+type PositionSet map[Position]struct{}
 
 func GetInput() string {
 	data, err := os.ReadFile("input/9")
@@ -17,8 +23,86 @@ func GetInput() string {
 	return string(data)
 }
 
+func getDelta(direction byte) (int, int) {
+	switch direction {
+	case 'L':
+		return -1, 0
+	case 'R':
+		return 1, 0
+	case 'U':
+		return 0, -1
+	case 'D':
+		return 0, 1
+	default:
+		log.Fatalf("Could not understand direction '%v'", direction)
+	}
+	return 0, 0
+}
+
+func inRange(x, y, tX, tY int) bool {
+	dX := tX - x
+	dY := tY - y
+
+	if dX < 0 {
+		dX *= -1
+	}
+
+	if dY < 0 {
+		dY *= -1
+	}
+
+	return dX <= 1 && dY <= 1
+}
+
+func followDelta(x, y, tX, tY int) (int, int) {
+	var dX, dY int
+
+	if tX < x {
+		dX = 1
+	} else if tX > x {
+		dX = -1
+	}
+
+	if tY < y {
+		dY = 1
+	} else if tY > y {
+		dY = -1
+	}
+
+	return dX, dY
+}
+
 func main() {
-  fmt.Println(GetInput())
+	lines := strings.Split(GetInput(), "\n")
+
+	var x, y, tX, tY int
+
+	visited := PositionSet{{
+		x: 0,
+		y: 0}: {}}
+
+	for _, line := range lines {
+		dX, dY := getDelta(line[0])
+		amount, err := strconv.ParseInt(line[2:], 10, 64)
+		if err != nil {
+			log.Fatalf("Cannot parse line '%v':\n%v", line, err)
+		}
+
+		// Movement of tail can be calculated for every multiple orthogonal steps
+		// The result will be the same
+		x += dX * int(amount)
+		y += dY * int(amount)
+
+    // Move the tail until in range
+		for !inRange(x, y, tX, tY) {
+			fX, fY := followDelta(x, y, tX, tY)
+			tX += fX
+			tY += fY
+			visited[Position{x: tX, y: tY}] = struct{}{}
+		}
+	}
+
 	fmt.Println("=-= PART 1 =-=")
+  fmt.Println(len(visited))
 	fmt.Println("=-= PART 2 =-=")
 }
