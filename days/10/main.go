a8d638a511a88aab792fb491fe452848e4692a5e
Damy Metzke <damy.metzke@gmail.com>
2022-12-10 12:07:09 +0100

    Solve day 10 part 2

--- days/10/main.go
+++ days/10/main.go
@@ -37,16 +37,33 @@ func main() {
   var signelStrength int
   cycle, counter := 1, 1
 
+  var image string
+
   for _, command := range commands {
     cycleDelta, counterDelta := getDelta(command)
 
     // I have to loop for each cycle delta,
     // in case the signal exists during an addx command
     for i := 0; i < cycleDelta; i++ {
+      pixelDiff := ((cycle - 1) % 40) - counter
+
+      // Draw pixel
+      if pixelDiff == -1 || pixelDiff == 0 || pixelDiff == 1 {
+        image+= "#"
+      } else {
+        image += "."
+      }
+
+      // Draw next line to screen
+      if cycle % 40 == 0 {
+        image += "\n"
+      }
+
       cycle++
       if i + 1 == cycleDelta {
         counter += counterDelta
       }
+
 
       if (cycle + 20) % 40 == 0 {
         fmt.Println(cycle)
@@ -59,4 +76,5 @@ func main() {
 	fmt.Println("=-= PART 1 =-=")
   fmt.Println(signelStrength)
 	fmt.Println("=-= PART 2 =-=")
+  fmt.Println(image)
 }
