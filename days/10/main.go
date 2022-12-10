a7f163c20512290c328f45cdbb282afb99863b7f
Damy Metzke <damy.metzke@gmail.com>
2022-12-10 11:57:40 +0100

    Solve day 10 part 1

--- days/10/main.go
+++ days/10/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func GetInput() string {
@@ -15,8 +17,46 @@ func GetInput() string {
 	return string(data)
 }
 
+// The first value is the cycle delta
+// The second value is the counter delta
+func getDelta(command string) (int, int) {
+  if command == "noop" {
+    return 1, 0;
+  }
+
+  counter, err := strconv.ParseInt(command[5:], 10, 64)
+  if err != nil {
+    log.Fatalf("Could not parse command '%v'", command)
+  }
+  return 2, int(counter)
+}
+
 func main() {
-  fmt.Println(GetInput())
+  commands := strings.Split(GetInput(), "\n")
+
+  var signelStrength int
+  cycle, counter := 1, 1
+
+  for _, command := range commands {
+    cycleDelta, counterDelta := getDelta(command)
+
+    // I have to loop for each cycle delta,
+    // in case the signal exists during an addx command
+    for i := 0; i < cycleDelta; i++ {
+      cycle++
+      if i + 1 == cycleDelta {
+        counter += counterDelta
+      }
+
+      if (cycle + 20) % 40 == 0 {
+        fmt.Println(cycle)
+        fmt.Println(command)
+        signelStrength += counter * cycle
+      }
+    }
+  }
+
 	fmt.Println("=-= PART 1 =-=")
+  fmt.Println(signelStrength)
 	fmt.Println("=-= PART 2 =-=")
 }
