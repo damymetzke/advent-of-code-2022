4bae6394635cb8112f3763a96b81d802e95a10d9
Damy Metzke <damy.metzke@gmail.com>
2022-12-11 12:47:54 +0100

    Solved day 11 part 1
    I've crashed my desktop twice

--- days/11/main.go
+++ days/11/main.go
@@ -24,6 +24,7 @@ type Monkey struct {
 	testDivisibleBy   int
 	resultTrueMonkey  int
 	resultFalseMonkey int
+	inspectedNum      int
 }
 
 func GetInput() string {
@@ -87,27 +88,28 @@ func ParseMonkey(lines []string, i int) Monkey {
 		log.Fatalf("Could not parse test divisible by\n  line: '%v'", lines[i+3])
 	}
 
-  // Parse result true monkey
-  resultTrueMonkey, err := strconv.ParseInt(lines[i+4][29:], 10, 64)
+	// Parse result true monkey
+	resultTrueMonkey, err := strconv.ParseInt(lines[i+4][29:], 10, 64)
 
 	if err != nil {
 		log.Fatalf("Could not parse result true monkey\n  line: '%v'", lines[i+4])
 	}
 
-  // Parse result false monkey
-  resultFalseMonkey, err := strconv.ParseInt(lines[i+5][30:], 10, 64)
+	// Parse result false monkey
+	resultFalseMonkey, err := strconv.ParseInt(lines[i+5][30:], 10, 64)
 
 	if err != nil {
 		log.Fatalf("Could not parse result false monkey\n  line: '%v'", lines[i+5])
 	}
 
 	return Monkey{
-		items:           items,
-		operationType:   operationType,
-		operationAmount: operationAmount,
-		testDivisibleBy: int(testDivisibleBy),
-    resultTrueMonkey: int(resultTrueMonkey),
-    resultFalseMonkey: int(resultFalseMonkey),
+		items:             items,
+		operationType:     operationType,
+		operationAmount:   operationAmount,
+		testDivisibleBy:   int(testDivisibleBy),
+		resultTrueMonkey:  int(resultTrueMonkey),
+		resultFalseMonkey: int(resultFalseMonkey),
+		inspectedNum:      0,
 	}
 }
 
@@ -131,7 +133,63 @@ func main() {
 
 	wait.Wait()
 
-	fmt.Println(monkeys)
+	// Go for 20 rounds
+	for i := 0; i < 20; i++ {
+		// Each monkey gets a round
+		for j, monkey := range monkeys {
+			// Consider each item
+			for _, item := range monkey.items {
+				// Transform worry
+				switch monkey.operationType {
+				case Multiply:
+					item *= monkey.operationAmount
+				case Add:
+					item += monkey.operationAmount
+				case Squared:
+					item = item * item
+				default:
+					log.Fatalf("Invalid state:\n%v", monkey)
+				}
+
+				// Monkey is bored
+				item /= 3
+
+				if item%monkey.testDivisibleBy == 0 {
+					monkeys[monkey.resultTrueMonkey].items = append(monkeys[monkey.resultTrueMonkey].items, item)
+				} else {
+					monkeys[monkey.resultFalseMonkey].items = append(monkeys[monkey.resultFalseMonkey].items, item)
+				}
+
+				// Increment inspectednum
+				monkey.inspectedNum++
+			}
+
+			monkey.items = []int{}
+			// I initially forgot to do this.
+			// This duplicated all items.
+			// And exponental growth is very real.
+			// This caused my memory to overflow and my desktop to crash.
+			// :)
+			monkeys[j] = monkey
+		}
+	}
+
+	maxInspected := [2]int{0, 0}
+
+	for _, monkey := range monkeys {
+		max := monkey.inspectedNum
+    if max > maxInspected[0] {
+      maxInspected[0], max = max, maxInspected[0]
+    }
+
+    if max > maxInspected[1] {
+      maxInspected[1] = max
+    }
+	}
+
+  monkeyBusiness := maxInspected[0] * maxInspected[1]
+
 	fmt.Println("=-= PART 1 =-=")
+	fmt.Println(monkeyBusiness)
 	fmt.Println("=-= PART 2 =-=")
 }
