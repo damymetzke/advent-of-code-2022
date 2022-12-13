3233f9a2be98573b9cc8f90e76c19c0e3ef8163c
Damy Metzke <damy.metzke@gmail.com>
2022-12-13 22:07:56 +0100

    Solution day 12 part 1
    Includes visualization at the end

--- days/12/main.go
+++ days/12/main.go
@@ -4,7 +4,14 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
+	"time"
 )
+
+type Position struct {
+	x int
+	y int
+}
 
 func GetInput() string {
 	data, err := os.ReadFile("input/12")
@@ -15,8 +22,144 @@ func GetInput() string {
 	return string(data)
 }
 
+func GetPossibleDirections(position Position) [4]Position {
+	return [4]Position{
+		{
+			x: position.x - 1,
+			y: position.y,
+		},
+		{
+			x: position.x + 1,
+			y: position.y,
+		},
+		{
+			x: position.x,
+			y: position.y - 1,
+		},
+		{
+			x: position.x,
+			y: position.y + 1,
+		},
+	}
+}
+
+func VisualizeGrid(grid [][]byte, visited [][]bool, clear bool) {
+	if clear {
+		for range grid {
+			fmt.Print("\x1b[1A\x1b[K")
+		}
+	}
+
+	for y, row := range grid {
+		for x, value := range row {
+			colorValue := 255 - ((value - 97) * 94)
+			if visited[y][x] {
+				fmt.Printf("\x1b[38;2;%v;255;%vm#\x1b[0m", colorValue, colorValue)
+			} else {
+				fmt.Printf("\x1b[38;2;255;%v;%vm.\x1b[0m", colorValue, colorValue)
+			}
+		}
+		fmt.Println()
+	}
+
+	time.Sleep(5 * time.Millisecond)
+
+}
+
+func FindShortestPath(start, end Position, grid [][]byte, visualize bool) int {
+	var steps int
+	currentPositions := []Position{start}
+	var nextPositions []Position
+
+	visited := make([][]bool, len(grid))
+	gridWidth := len(grid[0])
+	for i := range visited {
+		visited[i] = make([]bool, gridWidth)
+	}
+
+	if visualize {
+		VisualizeGrid(grid, visited, false)
+	}
+
+	visited[start.y][start.x] = true
+
+	for {
+		if len(currentPositions) == 0 {
+			break
+		}
+
+		steps++
+		for _, next := range currentPositions {
+			height := grid[next.y][next.x]
+			for _, possible := range GetPossibleDirections(next) {
+				// Check bounds
+				if possible.x < 0 || possible.x >= gridWidth || possible.y < 0 || possible.y >= len(grid) {
+					continue
+				}
+
+				// Check if already visited
+				if visited[possible.y][possible.x] == true {
+					continue
+				}
+
+				// Check if unreachable
+				if grid[possible.y][possible.x] > height+1 {
+					continue
+				}
+
+				// Check for end
+				if possible == end {
+					return steps
+				}
+
+				// Add next position
+				visited[possible.y][possible.x] = true
+				nextPositions = append(nextPositions, possible)
+			}
+		}
+
+		if visualize {
+			VisualizeGrid(grid, visited, true)
+		}
+		currentPositions = nextPositions
+		nextPositions = []Position{}
+	}
+
+	return 0
+}
+
 func main() {
-  fmt.Println(GetInput())
+	lines := strings.Split(GetInput(), "\n")
+	grid := make([][]byte, len(lines))
+
+	for i, line := range lines {
+		grid[i] = []byte(line)
+	}
+
+	var start, end Position
+
+	for y, row := range grid {
+		for x, value := range row {
+			if value == 'S' {
+				grid[y][x] = 'a'
+				start = Position{
+					x,
+					y,
+				}
+			} else if value == 'E' {
+				grid[y][x] = 'z'
+				end = Position{
+					x,
+					y,
+				}
+			}
+		}
+	}
+
 	fmt.Println("=-= PART 1 =-=")
+  fmt.Println(FindShortestPath(start, end, grid, false))
 	fmt.Println("=-= PART 2 =-=")
+
+	fmt.Println("=-= Visual =-=")
+	FindShortestPath(start, end, grid, true)
 }
