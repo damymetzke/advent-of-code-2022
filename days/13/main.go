f652cc577339ed282da15a1c81436c6dfc512c81
Damy Metzke <damy.metzke@gmail.com>
2022-12-14 19:48:09 +0100

    Implement parsing for day 13

--- days/13/main.go
+++ days/13/main.go
@@ -4,7 +4,16 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
+	"sync"
 )
+
+type Data struct {
+	isArray bool
+	value   int
+	array   []Data
+}
 
 func GetInput() string {
 	data, err := os.ReadFile("input/13")
@@ -15,8 +24,75 @@ func GetInput() string {
 	return string(data)
 }
 
+func ParseNumber(line string, offset int) (int, Data) {
+	end := offset
+	for line[end] != ',' && line[end] != ']' {
+		end++
+	}
+	parsed, err := strconv.ParseInt(line[offset:end], 10, 64)
+
+	if err != nil {
+		log.Fatalf("Could not parse integer from '%v'\n  with offset '%v'", line, offset)
+	}
+
+	return end, Data {
+    isArray: false,
+    value: int(parsed),
+    array: []Data{},
+  }
+}
+
+func ParseArray(line string, offset int) (int, Data) {
+	result := []Data{}
+
+	for line[offset] != ']' {
+    offset++
+    if line[offset] == ']' {
+      break
+    }
+		var value Data
+		if line[offset] == '[' {
+			offset, value = ParseArray(line, offset)
+
+		} else {
+			offset, value = ParseNumber(line, offset)
+		}
+
+		result = append(result, value)
+	}
+	return offset + 1, Data{
+		isArray: true,
+		value:   0,
+		array:   result,
+	}
+}
+
+func ParseLine(line string) Data {
+	_, result := ParseArray(line, 0)
+	return result
+}
+
 func main() {
-  fmt.Println(GetInput())
+	input := strings.Split(GetInput(), "\n")
+
+	var wait sync.WaitGroup
+
+	left := make([]Data, len(input)/3)
+	right := make([]Data, len(input)/3)
+
+	for i := 0; i < len(input)/3; i++ {
+		wait.Add(1)
+		num := i
+
+		go func() {
+			left[num] = ParseLine(input[3*num])
+			right[num] = ParseLine(input[3*num+1])
+			wait.Done()
+		}()
+	}
+
+	wait.Wait()
+
 	fmt.Println("=-= PART 1 =-=")
 	fmt.Println("=-= PART 2 =-=")
 }
