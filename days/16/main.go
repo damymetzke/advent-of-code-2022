c6dad80efd34a7a6f799b23cb94e0c5fdb079590
Damy Metzke <damy.metzke@gmail.com>
2022-12-16 12:18:11 +0100

    Parse data for day 16

--- days/16/main.go
+++ days/16/main.go
@@ -4,7 +4,15 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
+
+type Valve struct {
+	name  string
+	rate  int
+	edges []string
+}
 
 func GetInput() string {
 	data, err := os.ReadFile("input/16")
@@ -15,8 +23,51 @@ func GetInput() string {
 	return string(data)
 }
 
+func ParseLine(line string) Valve {
+  parts := strings.Split(line, "; ")
+  name := parts[0][6:8]
+  rate, rateErr := strconv.ParseInt(parts[0][23:], 10, 64)
+
+  end := strings.Split(parts[1], "valve")[1]
+  if end[0] == 's' {
+    end = end[2:]
+  } else {
+    end =end[1:]
+  }
+
+  edges := strings.Split(end, ", ")
+
+  if rateErr != nil {
+    log.Fatalf("Could not parse line '%v'", line)
+  }
+  return Valve {
+    name: name,
+    rate: int(rate),
+    edges: edges,
+  }
+}
+
 func main() {
-  fmt.Println(GetInput())
+  lines := strings.Split(GetInput(), "\n")
+
+  valveChannel := make(chan Valve)
+
+  for _, line := range lines {
+    value := line
+    go func(){
+      valveChannel <- ParseLine(value)
+    }()
+  }
+
+  valves := make([]Valve, len(lines))
+
+  for i := 0; i < len(valves); i++ {
+    valves[i] = <- valveChannel
+    fmt.Println(valves[i])
+  }
+
+
+
 	fmt.Println("=-= PART 1 =-=")
 	fmt.Println("=-= PART 2 =-=")
 }
