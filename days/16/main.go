fa49958231ae9b10a65f89b9c16f13cb3c3e2d2c
Damy Metzke <damy.metzke@gmail.com>
2022-12-16 17:08:30 +0100

    Build graph for day 16

--- days/16/main.go
+++ days/16/main.go
@@ -14,6 +14,21 @@ type Valve struct {
 	edges []string
 }
 
+type GraphEdge struct {
+	node  *GraphNode
+	steps int
+}
+
+type GraphNode struct {
+	valve Valve
+	edges []GraphEdge
+}
+
+type Graph struct {
+	root  *GraphNode
+	nodes []*GraphNode
+}
+
 func GetInput() string {
 	data, err := os.ReadFile("input/16")
 	if err != nil {
@@ -24,49 +39,81 @@ func GetInput() string {
 }
 
 func ParseLine(line string) Valve {
-  parts := strings.Split(line, "; ")
-  name := parts[0][6:8]
-  rate, rateErr := strconv.ParseInt(parts[0][23:], 10, 64)
+	parts := strings.Split(line, "; ")
+	name := parts[0][6:8]
+	rate, rateErr := strconv.ParseInt(parts[0][23:], 10, 64)
 
-  end := strings.Split(parts[1], "valve")[1]
-  if end[0] == 's' {
-    end = end[2:]
-  } else {
-    end =end[1:]
-  }
+	end := strings.Split(parts[1], "valve")[1]
+	if end[0] == 's' {
+		end = end[2:]
+	} else {
+		end = end[1:]
+	}
 
-  edges := strings.Split(end, ", ")
+	edges := strings.Split(end, ", ")
 
-  if rateErr != nil {
-    log.Fatalf("Could not parse line '%v'", line)
-  }
-  return Valve {
-    name: name,
-    rate: int(rate),
-    edges: edges,
+	if rateErr != nil {
+		log.Fatalf("Could not parse line '%v'", line)
+	}
+	return Valve{
+		name:  name,
+		rate:  int(rate),
+		edges: edges,
+	}
+}
+
+func UpdateGraph(graph *Graph, valve Valve) {
+	next := &GraphNode{
+		valve: valve,
+		edges: []GraphEdge{},
+	}
+
+	for _, edge := range valve.edges {
+		for i := range graph.nodes {
+			if graph.nodes[i].valve.name == edge {
+				graph.nodes[i].edges = append(graph.nodes[i].edges, GraphEdge{
+					node:  next,
+					steps: 1,
+				})
+
+				next.edges = append(next.edges, GraphEdge{
+					node:  graph.nodes[i],
+					steps: 1,
+				})
+			}
+		}
+	}
+
+	graph.nodes = append(graph.nodes, next)
+  if valve.name == "AA" {
+    graph.root = next
   }
 }
 
 func main() {
-  lines := strings.Split(GetInput(), "\n")
+	lines := strings.Split(GetInput(), "\n")
 
-  valveChannel := make(chan Valve)
+	valveChannel := make(chan Valve, len(lines))
 
-  for _, line := range lines {
-    value := line
-    go func(){
-      valveChannel <- ParseLine(value)
-    }()
+	for _, line := range lines {
+		value := line
+		go func() {
+			valveChannel <- ParseLine(value)
+		}()
+	}
+
+	valves := make([]Valve, len(lines))
+  var graph Graph
+
+	for i := 0; i < len(valves); i++ {
+		valves[i] = <-valveChannel
+    UpdateGraph(&graph, valves[i])
+	}
+
+  fmt.Println(*graph.root)
+  for _, node := range graph.nodes {
+    fmt.Println(*node)
   }
-
-  valves := make([]Valve, len(lines))
-
-  for i := 0; i < len(valves); i++ {
-    valves[i] = <- valveChannel
-    fmt.Println(valves[i])
-  }
-
-
 
 	fmt.Println("=-= PART 1 =-=")
 	fmt.Println("=-= PART 2 =-=")
