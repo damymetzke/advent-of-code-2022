82c916a9a896a3ced5adaceffd92ee8fa858885e
Damy Metzke <damy.metzke@gmail.com>
2022-12-21 12:59:19 +0100

    D17: finish display function

--- days/17/output/index.go
+++ days/17/output/index.go
@@ -8,7 +8,14 @@ import (
 )
 
 func Output(boardInput <-chan BoardDisplay) {
+  for i := 0; i < 40; i++ {
+    fmt.Println()
+  }
+
 	for board := range boardInput {
+    for i := 0; i < 40; i++ {
+      fmt.Print("\x1b[1A")
+    }
 		for _, line := range board {
 			for _, value := range line {
 				switch value {
@@ -22,8 +29,6 @@ func Output(boardInput <-chan BoardDisplay) {
 			}
 			fmt.Println()
 		}
-
-    fmt.Println()
 
     time.Sleep(1000 * time.Millisecond)
 	}
